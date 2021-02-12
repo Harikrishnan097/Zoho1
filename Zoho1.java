4c76529dbfb49d88945d993a823312902656e6f8
Harikrishnan <31622792+Harikrishnan097@users.noreply.github.com>
2021-02-12 09:32:55 +0530

    Add files via upload

---
+++ Zoho1.java
@@ -0,0 +1,35 @@
+package com.krishnan.arraysum;
+
+public class Zoho1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		String a= "WELCOME";
+		int mid = a.length()/2;
+		String midafter= a.substring(mid);
+		System.out.println(midafter);
+		StringBuilder b = new StringBuilder();
+		for (int i= 0 ; i<midafter.length() ;i++)
+		{
+			
+			
+			
+			char c = midafter.charAt(i) ;
+			
+			b.append(c);
+			
+			
+			System.out.println(b);
+			
+			
+		}
+		
+	}
+		
+	}
+
+
